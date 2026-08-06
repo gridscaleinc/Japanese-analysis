246cc7f71311149a1baa9089934e224f3058c83c
田芳 <tf199429@163.com>
2026-08-06 18:11:52 +0900

    Initial Commit

---
+++ JapaneseAnalysis/JapaneseAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JapaneseAnalysisApp.swift
+//  JapaneseAnalysis
+//
+//  Created by 田芳 on R 8/08/06.
+//
+
+import SwiftUI
+
+@main
+struct JapaneseAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
